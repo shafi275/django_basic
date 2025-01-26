a31ccbf962816867a6cc269eb1ea0462794698f5
shafi <shariyarshafi27@gmail.com>
2025-01-26 11:27:07 +0600

    return html template with the help of backend server

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hey i am a django lover....")
+    return render(request,"index.html")
 def success_page(request):
     return HttpResponse(" <h1>Hey this is a success page </h1>")
